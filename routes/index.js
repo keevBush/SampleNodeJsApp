4a9c269be6cfcdaeb1bcfe88529028c9471f98e5
Kevin Bush <keevbush@hotmail.com>
2022-07-04 13:01:10 +0200

    New modification

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Welcome to OKADEMY' });
+  res.render('index', { title: 'Welcome to OKADEMY at ITOT Africa' });
 });
 
 module.exports = router;
